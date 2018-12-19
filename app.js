7cc6001daf687ed43b43722c6c005b5571c89fb3
Greg Ng <>
2018-12-18 22:16:06 -0700

    use knuth fisher yates algorithm

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 const generateRandomNumbers = () => {
     const numList = []; // array for random numbers
     const max = 10000;
-    const min = 1;
-    let increment = 0;
     let num;
     let prevNum;
 
     // generate a array of numbers form 1 to 10000
     for (i = 0; i < max; i++) {
-        numList[i] = i + 1;
+        numList[i] = i + 1; // start at 1
     }
 
-    while (increment < max) {
-        num = Math.floor(Math.random() * max);
+    // using Knuth-Fisher-Yates shuffle algorithm
+    for (i = max - 1; i > 0; i--) {
+        num = Math.floor(Math.random() * (i + 1));
         prevNum = numList[num];
-        numList[num] = numList[increment];
-        numList[increment] = prevNum;
-        increment++;
+        numList[num] = numList[i];
+        numList[i] = prevNum;
     }
     return numList;
 }
