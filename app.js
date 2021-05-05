833e6b3442221f281e07b67e0c1b8e2fe6ede361
Greg Ng <gregng14@gmail.com>
2021-05-04 23:42:34 -0600

    Update random number generator with new tests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const generateRandomNumbers = () => {
+function randInt(max) {
+    return Math.floor(Math.random() * (max + 1));
+}
+
+function generateRandomNumbers(max, randFunc) {
     const numList = []; // array for random numbers
-    const max = 10000;
     let num;
     let prevNum;
 
@@ -11,7 +14,7 @@ const generateRandomNumbers = () => {
 
     // using Knuth-Fisher-Yates shuffle algorithm
     for (i = max - 1; i > 0; i--) {
-        num = Math.floor(Math.random() * (i + 1));
+        num = randFunc(i);
         prevNum = numList[num];
         numList[num] = numList[i];
         numList[i] = prevNum;
@@ -19,7 +22,22 @@ const generateRandomNumbers = () => {
     return numList;
 }
 
-const numList = generateRandomNumbers();
-numList.forEach(num => console.log(num));
-const numSet = new Set(numList);
-console.assert(numSet.size === 10000, 'List of randomly generated numbers is not unique');
+const numList = generateRandomNumbers(10000, randInt);
+console.log(numList)
+
+/* TESTING */
+
+// used for testing
+// mocks randInt(max)
+function randIntMock(max) {
+    const staticArray = [0,0,1,1,4,3]
+    return staticArray[max];
+}
+
+const numTestList = generateRandomNumbers(6, randIntMock);
+console.assert(numTestList.toString() === '3,1,6,2,5,4', `List should be 3,1,6,2,5,4, result is ${numTestList}`)
+numTestList.forEach(num => {
+    console.assert( 0 < num <= 6, `${num} is not a number between 1 and 6`);
+});
+const numSet = new Set(numTestList);
+console.assert(numSet.size === 6, 'List of randomly generated numbers is not unique');
